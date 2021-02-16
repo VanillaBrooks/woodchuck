4794cd7e3043e5a71b5291eb1b1c94bcf2346c5b
VanillaBrooks <brooks@karlik.org>
2021-02-15 22:44:59 -0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+use std::io::prelude::*;
+
+pub struct LatexTable<T>
+where
+    T: Read,
+{
+    pub reader: csv::Reader<T>,
+}
+impl<T> LatexTable<T>
+where
+    T: Read,
+{
+    pub fn from_reader(reader: T) -> Self {
+        let reader = csv::Reader::from_reader(reader);
+        let s = LatexTable { reader };
+        s
+    }
+
+    fn column_spacings(&mut self) -> Result<String, csv::Error> {
+        let record = self.reader.headers()?;
+        let length = record.len();
+        let mut out = String::with_capacity(1 + length * 2);
+
+        out.push('|');
+        for _ in 0..length {
+            out.push_str("c|")
+        }
+
+        Ok(out)
+    }
+
+    fn column_headers(&mut self) -> Result<String, csv::Error> {
+        let mut buffer = String::new();
+        let record = self.reader.headers()?;
+        row_sequence(&mut buffer, record);
+        Ok(buffer)
+    }
+
+    pub fn make_header(&mut self) -> Result<String, csv::Error> {
+        let table = "\\begin{table}[H]\n";
+        let center = "\t\\begin{center}\n";
+        let tabular = "\t\t\\begin{tabular} { ";
+        let inner = self.column_spacings()?;
+        let outer = " }\n";
+
+        let mut out =
+            String::with_capacity(center.len() + tabular.len() + inner.len() + outer.len());
+        out.push_str(table);
+        out.push_str(center);
+        out.push_str(tabular);
+        out.push_str(&inner);
+        out.push_str(outer);
+
+        Ok(out)
+    }
+
+    pub fn make_ender(&self) -> String {
+        let table = "\\end{table}\n";
+        let extras = self.table_extras();
+        let tabular = "\t\t\\end{tabular}\n";
+        let center = "\t\\end{center}\n";
+        let mut out =
+            String::with_capacity(extras.len() + table.len() + tabular.len() + center.len());
+
+        out.push_str(tabular);
+        out.push_str(extras);
+        out.push_str(center);
+        out.push_str(table);
+        out
+    }
+
+    pub fn make_table(&mut self) -> Result<String, csv::Error> {
+        let mut out = String::new();
+
+        for row in self.reader.records() {
+            let row = row?;
+            row_sequence(&mut out, &row);
+        }
+
+        Ok(out)
+    }
+
+    fn table_extras(&self) -> &'static str {
+        "\t\t\\caption{Caption Here}\n\t\t\\label{labelhere}\n"
+    }
+
+    pub fn to_writer<W: Write>(&mut self, mut writer: W) -> Result<(), csv::Error> {
+        writer.write_all(self.make_header()?.as_bytes())?;
+        writer.write_all(self.column_headers()?.as_bytes())?;
+        writer.write_all(self.make_table()?.as_bytes())?;
+        writer.write_all(self.make_ender().as_bytes())?;
+        Ok(())
+    }
+}
+
+fn row_sequence(buffer: &mut String, row: &csv::StringRecord) {
+    let mut first = true;
+    for i in row.into_iter() {
+        if first {
+            buffer.push_str(&format!("\t\t\t{}", i));
+            first = false;
+        } else {
+            buffer.push_str(&format!(" & {}", i))
+        }
+    }
+    buffer.push_str("\\\\ \\hline \n");
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use clap::{App, Arg};
+use woodchuck::LatexTable;
+
+fn main() {
+    let app = make_cli();
+    let matches = app.get_matches();
+
+    let input = if let Some(x) = matches.value_of("csv") {
+        x
+    } else {
+        panic!("Was expecting a path to csv input file")
+    };
+
+    let output = if let Some(x) = matches.value_of("latex") {
+        x
+    } else {
+        panic!("Was expecting a path to csv input file")
+    };
+
+    // open the input file
+    let f = std::fs::File::open(input).expect("Could not open the input csv");
+    let mut table = LatexTable::from_reader(f);
+
+    // open the tex file to write to
+    let mut writer =
+        std::fs::File::create(output).expect("Could not open / create the output file");
+
+    table
+        .to_writer(&mut writer)
+        .expect("Could not write the result to file");
+}
+
+fn make_cli() -> App<'static, 'static> {
+    App::new("Woodchuck")
+        .version("0.1.0")
+        .author("Brooks")
+        .about("Convert CSV files to latex tables")
+        .arg(
+            Arg::with_name("csv")
+                .value_name("FILE")
+                .help("Input path to csv")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("latex")
+                .value_name("FILE")
+                .help("Path to .tex file to write to")
+                .takes_value(true),
+        )
+}
